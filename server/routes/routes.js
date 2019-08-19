f3a435350d6f9ff544841855c8b7e84595347387
Nanna <5608562@RTS-365.dk>
2019-08-19 08:43:33 +0200

    commit css

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,27 +4,11 @@ const mysql = require('../config/mysql');
 module.exports = (app) => {
     // herinde laver du dine routes via app
 
-    // Hvis det er dataudtræk som skal gentages, lav en async function, som kaldes nede i routen
 
-    // async function eksempel
-    async function getCategories() {
-        let database = await mysql.connect();
-        let [categories] = await database.execute(`
-            SELECT *
-            FROM newscategories
-            ORDER BY newsCategoryTitle ASC`);
-        database.end();
-        return categories;
-    }
 
     app.get('/', function(req, res) {
 
-        // den simpleste type route
-        res.send("noget");
-
-        // Det typiske route, når man renderer siden
         res.render('home', {
-            "title" : "The News Paper - News & Lifestyle Magazine Template",
         });
 
     }); // route slutter
