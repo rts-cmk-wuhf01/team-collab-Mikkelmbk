5bcf4d90e788d77532eb6c99c080708da6c4ffb7
Mikkel Back Koll <217236@RTS-365.dk>
2019-08-21 12:21:32 +0200

    slideren virker med funktionalitet, der mangler navn samt beskrivelse når man holder musen over samt at den også skal standse slideshowet når man holder musen over.

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -1,29 +1,48 @@
-document.addEventListener('DOMContentLoaded',()=>{
-	console.log("hej Verden");
+document.addEventListener('DOMContentLoaded', () => {
+
 
 
 	let contentWrapperElement = document.querySelector('.content-wrapper');
-
 	let sliderContainerElement = document.querySelector('.slider-container');
-	sliderContainerElement.style.width = (contentWrapperElement.offsetWidth-25)+`px`;
-	
+	sliderContainerElement.style.width = (contentWrapperElement.offsetWidth - 20) + `px`; // using the contentwrapperelements width to determine how wide the visual container of the slider should be by using offsetWidth
 
-	let variableSliderWidth = contentWrapperElement.offsetWidth -25;
+
+	let variableSliderWidth = contentWrapperElement.offsetWidth - 20; // giving the variablesliderwidth the same width as the visual slider container to later use that width to divide by the amount of images you want shown in the visual slider
 	let sliderItemElements = document.querySelectorAll('.slider-item');
 	let sliderSizeRegulatorElement = document.querySelector('.slider-size-regulator');
 
-	console.log(variableSliderWidth);
 
+	sliderItemElements.forEach((sliderItemElement) => {
 
-	sliderItemElements.forEach((sliderItemElement)=>{
-		// sliderItemElement.style.backgroundColor = "green";
-		// sliderItemElement.style.border = "1px solid yellow";
-		
-		
-
-		sliderItemElement.style.width = variableSliderWidth/3+`px`;
+		sliderItemElement.style.width = variableSliderWidth / 3 + `px`; // dividing variablesliderwidth by 3 to display 3 images on the visual slider
 	})
 
-	sliderSizeRegulatorElement.style.width = ((variableSliderWidth/3) * sliderItemElements.length)+`px`;
+	sliderSizeRegulatorElement.style.width = ((variableSliderWidth / 3) * sliderItemElements.length) + `px`; // finds the width of a single slider item and times it by the amount of slider items in total to give the hiden slider-item container it's width.
+
+	slideImagesByInterval();
+
+
+
+
+	function slideImagesByInterval() {
+
+		let sliderMovedDistance = variableSliderWidth / 3;
+
+		sliderSizeRegulatorElement.addEventListener('transitionend', () => {
+			sliderSizeRegulatorElement.style.transition = "unset";
+			let firstChild = sliderSizeRegulatorElement.removeChild(sliderSizeRegulatorElement.childNodes[0]);
+			sliderSizeRegulatorElement.insertAdjacentElement('beforeend', firstChild);
+			sliderSizeRegulatorElement.style.marginLeft = 0;
+
+		})
+		setInterval(() => {
+			sliderSizeRegulatorElement.style.transition = "all 0.3s";
+			sliderSizeRegulatorElement.style.marginLeft = -sliderMovedDistance + 'px';
+
+		}, 2500)
+
+
+
+	}
 
 })
