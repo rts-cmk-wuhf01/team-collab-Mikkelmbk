25cf8cfcfdaf92eeb9bf8f7940b4d5f4de59974c
Mikkel Back Koll <217236@RTS-365.dk>
2019-08-22 10:44:44 +0200

    fjernede gamle console logs osv

--- public/js/movie-overlay.js
+++ public/js/movie-overlay.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded',()=>{
 	let movieOverlayElements = document.querySelectorAll('.movie-overlay');
-	console.log(movieOverlayElements)
 
 	movieOverlayElements.forEach((movieOverlayElement)=>{
-		console.log(movieOverlayElement);
 
 		movieOverlayElement.addEventListener('mouseover',()=>{
 			movieOverlayElement.style.transition = "opacity 0.5s";
